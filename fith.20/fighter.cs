85927f7fb9e3fb167f75082e41c1ff4d6b169dc1
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2022-09-28 12:47:23 +0200

    commit i mitten

--- fith.20/fighter.cs
+++ fith.20/fighter.cs
@@ -8,4 +8,16 @@ public string name;
 public int hp = 100;
 
 public wepons vapen;
+
+public void attack(fighter mål)
+{
+
+int damage = vapen.GetDamage();
+mål.hp -= damage;
+mål.hp  = Math.Max
+
+
+}
+
+
 }
